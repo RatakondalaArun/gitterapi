07c969bc90e46e1eb0221dabb78b2196875008f2
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-12-21 23:56:45 +0530

    added tests for user-resources

---
+++ test/src/v1/user_resource_test.dart
@@ -0,0 +1,60 @@
+import 'package:test/test.dart';
+
+import 'package:gitterapi/gitter_api.dart';
+import 'package:dotenv/dotenv.dart' as env;
+
+import '../../test_utils/utils.dart' show getFromEnv;
+
+void main() {
+  group('This tests UserResourse class', () {
+    env.load();
+    ApiKeys keys = ApiKeys.fromEnv();
+    if (keys.authToken == null || keys.authToken.isEmpty) {
+      keys = ApiKeys(env.env['AUTH_TOKEN']);
+    }
+    final userId = getFromEnv('USER_ID');
+    final roomId = getFromEnv('ROOM_ID');
+
+    final api = GitterApi(keys);
+    final userResource = api.v1.userResource;
+
+    test('Should Return current user as Map using me()', () async {
+      final result = await userResource.me();
+      expect(result is Map, true);
+    });
+
+    test('Should Return current user as Map using getCurrentUser()', () async {
+      final result = await userResource.getCurrentUser();
+      expect(result is Map, true);
+    });
+
+    test('Should get current rooms as List', () async {
+      final result = await userResource.getRooms(userId);
+      expect(result is List, true);
+    });
+
+    // TODO(@RatakondalaArun): hideRoom()
+
+    test('Should get UnreadItems as Map', () async {
+      final result = await userResource.getUnreadItems(userId, roomId);
+      expect(result is Map, result);
+    });
+
+    // TODO(@RatakondalaArun): markUnReadItemsAsRead
+
+    test('Should get Orgs as List', () async {
+      final result = await userResource.getOrgs(userId);
+      expect(result is List, true);
+    });
+
+    test('Should get Repos as List', () async {
+      final result = await userResource.getRepos(userId);
+      expect(result is List, true);
+    });
+
+    test('Should get Channels as List', () async {
+      final result = await userResource.getRepos(userId);
+      expect(result is List, true);
+    });
+  });
+}
