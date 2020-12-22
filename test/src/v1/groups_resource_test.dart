299d2a4b395d2e61d7144c9abedf7f9183c780a3
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-12-22 23:08:51 +0530

    updated tests

--- test/src/v1/groups_resource_test.dart
+++ test/src/v1/groups_resource_test.dart
@@ -6,15 +6,21 @@ import '../../test_utils/utils.dart';
 void main() {
   group('This tests Groups Resources methods', () {
     final keys = ApiKeys(getFromEnv('AUTH_TOKEN'));
-    final userId = getFromEnv('USER_ID');
-    final roomId = getFromEnv('ROOM_ID');
+    // final userId = getFromEnv('USER_ID');
+    // final roomId = getFromEnv('ROOM_ID');
     // final messageId = getFromEnv('MESSAGE_ID');
 
     final api = GitterApi(keys);
     final groupResources = api.v1.groupResource;
     test('Should get Get the groups current user is in as List', () async {
       final result = await groupResources.getGropus();
-      print(result);
+      expect(true, result is List);
+    });
+
+    test('Should get ', () async {
+      final groups = await groupResources.getGropus();
+      final groupId = groups[0]['id'];
+      final result = await groupResources.getRooms(groupId);
       expect(true, result is List);
     });
   });
