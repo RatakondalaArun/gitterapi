c45b7e26666590bb28d36082413ae227527cf482
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-12-22 15:30:23 +0530

    added MessagesResource class

---
+++ lib/src/v1/messages_resource.dart
@@ -0,0 +1,9 @@
+part of gitterapi;
+
+class MessagesResource extends Resource<V1> {
+  MessagesResource(V1 v) : super(v);
+
+  @override
+  // TODO: implement path
+  String get path => throw UnimplementedError();
+}
