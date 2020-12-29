cfc31f5433ec82fca81b312e1e3e8ff2bee3b98b
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-12-29 16:39:47 +0530

    added Result model

--- lib/models.dart
+++ lib/models.dart
@@ -1,8 +1,9 @@
-export 'models/user.dart';
-export 'models/room.dart';
-export 'models/provider.dart';
-export 'models/message.dart';
+export 'models/group.dart';
 export 'models/issue.dart';
 export 'models/mention.dart';
-export 'models/group.dart';
+export 'models/message.dart';
 export 'models/permission.dart';
+export 'models/provider.dart';
+export 'models/result.dart';
+export 'models/room.dart';
+export 'models/user.dart';
